82812190b5862c5a2f7339e7cba79adbf3976374
Paul <thatgreenguy@techie.com>
2018-03-16 22:46:25 +0000

    Connects to Oracle12c Database

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 'use strict'
 
-const config = require('config')
 const oracledb = require('oracledb')
 
-const CFG = config.get('CFG')
-
-let credentials = CFG.CREDENTIALS
-credentials.user = process.env.db_user
-credentials.password = process.env.db_password
-credentials.host = process.env.db_host
+let credentials = {}
+credentials.user = process.env.DB_USER
+credentials.password = process.env.DB_PASSWORD
+credentials.connectString = `${process.env.DB_PORT_1521_TCP_ADDR}/${process.env.DB_ENV_ORACLE_SID}`
 
 oracledb.getConnection(credentials, function (err, cn) {
   if (err) throw new Error('DB Connection failed: ' + err)
